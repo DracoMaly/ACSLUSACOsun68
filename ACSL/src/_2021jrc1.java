d9fcec35bc0ce474de1fff0976565fe16268d716
DracoMaly <dragondrake9000@gmail.com>
2022-07-08 20:48:31 -0700

    finished 2021 jr competition 1

--- ACSL/src/_2021jrc1.java
+++ ACSL/src/_2021jrc1.java
@@ -25,10 +25,36 @@ public class _2021jrc1 {
 	}
 	
 	public static void solve() {
+		int sequenceSize = r * (r-1)/2; 
 		
+		int num = transform(s);
+		int delta = d; 
+		for(int i = 1; i < sequenceSize; i++) {
+			num += delta;
+			num = transform(num); 
+		}
+		int row = r;
+		
+		for(int i = 0; i < row; i++) {
+			num += delta;
+			num = transform(num); 
+			output += num;
+		}
 	}
 	
 	public static void output() {
 		System.out.println(output);
+	}
+	
+	public static int transform(int a) {
+		while(a > 9) {
+			int sum = 0;
+			while(a > 0) {
+				sum += a % 10;
+				a /= 10;
+			}
+			a = sum;
+		}
+		return a;
 	}
 }
